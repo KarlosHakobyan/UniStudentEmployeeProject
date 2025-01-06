0032ffde636bf65bdf31193e00e87f418078f69d
Karlos <karloshakobyan99@gmail.com>
2025-01-06 23:37:58 +0400

    Add Exception handling

---
+++ AsyncACATest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AsyncACATest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+           await Run();
+        }
+
+        static async Task Run()
+        {
+            Console.WriteLine("Start");
+            throw new Exception("Sxal");
+        }
+    }
+}
